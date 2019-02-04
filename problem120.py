7c2f0677320e02901eab2064a28ce2f386fa4e0f
Wishu <Wishu969@gmail.com>
2019-02-04 10:17:03 +0100

    added problem 120 as test

---
+++ problem120.py
@@ -0,0 +1,14 @@
+def func(a,n):
+    return ((a-1)**n + (a+1)**n)%a**2
+
+maxtotal = 0
+for k in range (3,1001):
+    maxn = 0
+    for i in range(1,1500):
+        temp = func(k,i)
+        if temp > maxn:
+            maxn = temp
+    print('for {} max number is {}'.format(k,maxn))
+    maxtotal = maxtotal + maxn
+print('SUM: {}'.format(maxtotal))
+#333082500
