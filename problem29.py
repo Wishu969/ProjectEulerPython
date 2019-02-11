d443aea98a91f33e33769fa467af857a8f427e98
Wishu <Wishu969@gmail.com>
2019-02-11 23:19:25 +0100

    list are now sorted

--- problem29.py
+++ problem29.py
@@ -2,6 +2,8 @@ distinct = []
 
 for a in range(2,6):
     for b in range(2,6):
-        distinct.append(a**b)
-
+        if a**b not in distinct:
+            distinct.append(a**b)
+            
+distinct.sort()
 print(distinct)
