919c520365fbc4410769e787f415a474d85824a0
Wishu <Wishu969@gmail.com>
2019-02-12 00:28:29 +0100

    implemented octa tree, problem with appropriate datastructure

--- problem31.py
+++ problem31.py
@@ -1,5 +1,32 @@
 #unsolved
-#use octa tree
-#insert in list of lists
+tree = [200,100,50,20,10,5,2,1]
+totallist = []
+
+templist = set()
+
+#USE SET 
+
+def traverse(node,coin):
+    coin += tree[node]
+    templist.add(tree[node])
+    if coin == 200:
+        print(templist)
+        if templist not in totallist:
+            totallist.append(templist)
+            print(totallist)
+        templist.discard(tree[node])
+        coin -= tree[node]
+        return 0
+    elif coin > 200:
+        templist.discard(tree[node])
+        coin -= tree[node]
+        return 0
+    else:
+        traverse(1,coin)
+        traverse(2,coin)
+
+coin = 0
+traverse(1,coin)
+print(totallist)
 #compare and delete duplicates
 #find length of list
