9e4213a360dbd6e7f34dae47929f1b653ef465c2
Wishu <Wishu969@gmail.com>
2019-02-12 21:47:13 +0100

    unfinished

--- problem50.py
+++ problem50.py
@@ -3,14 +3,27 @@ def isPrime(n = 2):
         if n % num == 0:
             return 0
     return n
+def generator(n = 2):
+    temp = 0
+    while(True):
+        if isPrime(n) != 0: 
+            #temp += n
+            yield n
+        n += 1
 
+primelist = []
 temp = 0
-totalprime = 0
-cnt = 1
-while temp < 100:
-    cnt += 1
-    temp += isPrime(cnt)
-    if isPrime(temp) != 0:
-        totalprime = temp
+for i in generator():
+    temp += i
+    if temp > 100: break
+    primelist.append(i)
 
-print(totalprime)
+primelist.reverse()
+e = 0
+total = 0
+for prime in primelist:
+    total += prime
+    if isPrime(total) != 0: e = total
+
+
+print(e)
