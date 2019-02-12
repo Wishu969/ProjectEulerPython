c70dd27a19362b99d2d53f283e58ae2b05db9673
Wishu <Wishu969@gmail.com>
2019-02-13 00:32:46 +0100

    using multiprocessing for mapreduce

--- problem145.py
+++ problem145.py
@@ -1,6 +1,5 @@
-# unfinished code 
-#print does not equel example on project euler
-#10,30,50,70,90 not allowed
+from multiprocessing import Pool
+
 def reverse(n):
     return int(str(n)[::-1])
 
@@ -10,15 +9,27 @@ def isUneven(n):
             return False     
     return True
 
-x = 0
-a = 0
-for i in range(10**9):
-    if i % 10 != 0:
-    #print('{} + {} = {} ... {}'.format(i,reverse(i), i+reverse(i), isUneven(i+reverse(i))))
-        a = i + reverse(i)
-        if isUneven(a):
-            print('{}......{}'.format(i,a))
-            x += 1
+def test(n):
+    r = 0
+    for i in range(n-10000,n):
+        r += i
+    return r
 
-print(x)
+a = 0
+def thread(limit):
+    x = 0
+    for i in range((int)(limit/10),limit):
+        if i % 10 != 0:
+            a = i + reverse(i)
+            if isUneven(a):
+                x += 1
+    print(x)
+    return x
+
 #improve algorithm 
+a = 0
+if __name__ == '__main__':
+    p = Pool(5)
+    for i in p.map(thread,[10**1,10**2,10**3,10**4,10**5,10**6,10**7,10**8,10**9]):
+        a += i
+    print(a)
