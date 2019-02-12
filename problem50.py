502ed1c19f75d913cc7bba258f8f91defb552898
Wishu <Wishu969@gmail.com>
2019-02-12 21:08:00 +0100

    possible solution found

--- problem50.py
+++ problem50.py
@@ -1,8 +1,16 @@
 def isPrime(n = 2):
-    for i in range(2,(int)(n**0.5) +1):
-        if n % i == 0:
+    for num in range(2,(int)(n**0.5) +1):
+        if n % num == 0:
             return 0
     return n
 
+temp = 0
+totalprime = 0
+cnt = 1
+while temp < 100:
+    cnt += 1
+    temp += isPrime(cnt)
+    if isPrime(temp) != 0:
+        totalprime = temp
 
-print(isPrime(3))
+print(totalprime)
