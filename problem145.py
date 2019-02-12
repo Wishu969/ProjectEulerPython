eb87bd9a004e7e092d60c4daf08a74a302e6a349
Wishu <Wishu969@gmail.com>
2019-02-12 23:58:54 +0100

    found possible solution

--- problem145.py
+++ problem145.py
@@ -1,5 +1,6 @@
 # unfinished code 
 #print does not equel example on project euler
+#10,30,50,70,90 not allowed
 def reverse(n):
     return int(str(n)[::-1])
 
@@ -11,10 +12,11 @@ def isUneven(n):
 
 x = 0
 for i in range(10**3):
+    if i % 10 != 0:
     #print('{} + {} = {} ... {}'.format(i,reverse(i), i+reverse(i), isUneven(i+reverse(i))))
-    if isUneven(i + reverse(i)):
-        print('{}......{}'.format(i,i+reverse(i)))
-        x += 1
+        if isUneven(i + reverse(i)):
+            print('{}......{}'.format(i,i+reverse(i)))
+            x += 1
 
 print(x)
 #improve algorithm 
