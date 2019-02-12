dc222a5420bbd9f898048981e01a241132434b77
Wishu <Wishu969@gmail.com>
2019-02-12 22:01:04 +0100

    found solution, problem56 solved

--- problem56.py
+++ problem56.py
@@ -1,3 +1,12 @@
 #Considering natural numbers of the form, ab, 
 # where a, b < 100, what is the maximum digital sum?
-
+maxsum = 0
+temp = 0
+for a in range(1,100):
+    for b in range(1,100):
+        temp = a ** b
+        total = 0
+        for char in (str)(temp):
+            total += (int)(char)
+        if maxsum < total: maxsum = total
+print(maxsum)
