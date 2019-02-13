deb6023f36ce7647523ad0a8846d76a2196b22f3
Wishu <Wishu969@gmail.com>
2019-02-13 18:42:55 +0100

    created new datastructure "datalist"

--- problem31.py
+++ problem31.py
@@ -1,17 +1,20 @@
 #unsolved
 tree = [200,100,50,20,10,5,2,1]
-totallist = []
-templist = set()
 #find good datastructure
 #should be able to compare
 #nested structure allowed
 #must have push(),pop() or append(),remove()
-
-
-
-def traverse(node,coin):
+#USE TUPLE AND LIST
+class datalist:
+    def __init__(self): self.datalist = tuple()
+    def push(self,n): self.datalist += (n, )
+    def pop(self): self.datalist = tuple(list(self.datalist).pop())
+    def sort(self): self.datalist = tuple(list(self.datalist).sort())
+    def contains(self,n): return True if n in list(datalist) else False
+        
+def traverse(node,coin = 0,templist = tuple(),totallist = tuple()):
     coin += tree[node]
-    templist.add(tree[node])
+    templist += (tree[node], )
     if coin == 200:
         print(templist)
         if templist not in totallist:
@@ -25,11 +28,11 @@ def traverse(node,coin):
         coin -= tree[node]
         return 0
     else:
-        traverse(1,coin)
-        traverse(2,coin)
+        traverse(1,coin,templist,totallist)
+        traverse(2,coin,templist,totallist)
 
-coin = 0
-traverse(1,coin)
-print(totallist)
+
+traverse(1)
+
 #compare and delete duplicates
 #find length of list
