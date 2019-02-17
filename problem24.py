0c8d59c4fc0259685503d2d687a99c6a02e53999
Wishu <Wishu969@gmail.com>
2019-02-18 00:27:47 +0100

    added unfinished permutation function

--- problem24.py
+++ problem24.py
@@ -1,1 +1,17 @@
 #milionth lexicographic permutation
+def perm(n):
+    perml = ()
+    numlist = tuple(str(n))
+    x = len(numlist)
+    a = x-1
+    perml.__add__(x)
+    return x
+
+
+print(perm('0123'))
+#012
+#021
+#102
+#120
+#201
+#210
