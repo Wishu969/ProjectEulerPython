bf91ab3b2e7443d87ed360e1bfe85bb647ae47d3
Wishu <Wishu969@gmail.com>
2019-02-18 20:02:25 +0100

    added new function for algorithm that takes every possible combination in list

--- problem50.py
+++ problem50.py
@@ -1,11 +1,11 @@
 #unfinished
 def isPrime(n = 2):
-    for num in range(2,(int)(n**0.5) +1):
+    for num in range(2,(int)(n**0.5) + 1):
         if n % num == 0:
             return 0
     return n
+
 def generator(n = 2):
-    temp = 0
     while(True):
         if isPrime(n) != 0: 
             #temp += n
@@ -16,15 +16,20 @@ primelist = []
 temp = 0
 for i in generator():
     temp += i
-    if temp > 100: break
+    if temp > 10000: break
     primelist.append(i)
+print(primelist)
 
-primelist.reverse()
-e = 0
-total = 0
-for prime in primelist:
-    total += prime
-    if isPrime(total) != 0: e = total
+def func():
+    length = len(primelist) - 1
+    temp = 0
+    cnt = 0
+    for i in range(length):
+        temp += i
+        cnt += 1
+    if isPrime(temp) != 0:
+        print(temp)
+        print(cnt)
+    
 
-
-print(e)
+func()
