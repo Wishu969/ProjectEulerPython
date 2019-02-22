e2bf9476250092f420fcbcacb96bfa9c6448bc4e
Wishu <Wishu969@gmail.com>
2019-02-22 19:46:50 +0100

    find new algorithm

--- problem23.py
+++ problem23.py
@@ -4,16 +4,18 @@
 def isabundant(n):
     temp = 0
     for i in range(1,(int)(n/2) + 1):
-        if n % i == 0: temp += i
-    return True if temp > n else False
+        if n % i == 0: 
+            temp += i
+    return temp > n 
 
 templist = []
 a = 0
-b = 0
+b = 28124
 x = 0
 while b < 28123:
     if isabundant(x):
         templist.append(x)
         a,b = b,b+a
     x += 1
+print(isabundant(12))
 print(templist)
