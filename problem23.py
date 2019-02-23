101029362f6e24f73b459793a963dc90b51c2bdd
Wishu <Wishu969@gmail.com>
2019-02-23 21:40:42 +0100

    found algorithm

--- problem23.py
+++ problem23.py
@@ -1,21 +1,30 @@
 #proper divisor
 #limit 28123
 #sum of TWO numbers
-def isabundant(n):
-    temp = 0
-    for i in range(1,(int)(n/2) + 1):
-        if n % i == 0: 
-            temp += i
-    return temp > n 
+def isabundant(n = 1):
+    while n < 200:
+        temp = 0
+        for i in range(1,(int)(n/2) + 1):
+            temp = temp + i if n % i == 0 else temp + 0
+        if temp > n: 
+            yield n
+        n += 1
 
-templist = []
-a = 0
-b = 28124
-x = 0
-while b < 28123:
-    if isabundant(x):
-        templist.append(x)
-        a,b = b,b+a
-    x += 1
-print(isabundant(12))
-print(templist)
+full = []
+for i in isabundant():
+    full.append(i)
+print(full)
+
+def search(n, l):
+    y = len(l) - 1
+    x = 0
+    while l[x] + l[y] != n:
+        print('{} {} = {}'.format(x,y,l[x] + l[y]))
+        if l[x] == l[y]: return False
+        if l[x] + l[y] > n: y -= 1
+        if l[x] + l[y] < n: x += 1
+    return True
+
+for i in range(1,28123):
+    if search(i,full):
+        
