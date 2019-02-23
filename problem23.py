030a6d8ca12f88b5d394642c93e402e341299cc5
Wishu <Wishu969@gmail.com>
2019-02-23 22:04:30 +0100

    algorithm works, abundent function inefficient

--- problem23.py
+++ problem23.py
@@ -2,7 +2,7 @@
 #limit 28123
 #sum of TWO numbers
 def isabundant(n = 1):
-    while n < 200:
+    while n < 15000:
         temp = 0
         for i in range(1,(int)(n/2) + 1):
             temp = temp + i if n % i == 0 else temp + 0
@@ -13,18 +13,24 @@ def isabundant(n = 1):
 full = []
 for i in isabundant():
     full.append(i)
-print(full)
 
 def search(n, l):
-    y = len(l) - 1
+    y = 390
     x = 0
     while l[x] + l[y] != n:
-        print('{} {} = {}'.format(x,y,l[x] + l[y]))
-        if l[x] == l[y]: return False
+        #print('{} {} = {}'.format(x,y,l[x] + l[y]))
+        if l[x] == l[y]: return True
+        if x > y: return True
         if l[x] + l[y] > n: y -= 1
         if l[x] + l[y] < n: x += 1
-    return True
+    return False
 
-for i in range(1,28123):
+test = 0
+for i in range(1,1):
     if search(i,full):
-        
+        print(i)
+        test += 1
+print(test)
+
+print(search(28124,full))
+print(full)
