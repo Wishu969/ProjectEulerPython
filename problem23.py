467763bb3b0166e8423aea8a5047dd474f852f33
Wishu <Wishu969@gmail.com>
2019-02-26 00:21:23 +0100

    added database abundant numbers for efficienty

--- problem23.py
+++ problem23.py
@@ -1,21 +1,28 @@
 #proper divisor
 #limit 28123
 #sum of TWO numbers
+with open(r'D:\Python\Courses\ProjectEulergithub\Database\databaseProblem23') as file:
+    lines = file.read().splitlines()
+lines = list(map(int, lines))
+
 def isabundant(n = 1):
-    while n < 15000:
+    while n < 30000:
         temp = 0
         for i in range(1,(int)(n/2) + 1):
             temp = temp + i if n % i == 0 else temp + 0
         if temp > n: 
+            print(n, temp)
             yield n
         n += 1
 
 full = []
-for i in isabundant():
-    full.append(i)
+def insert_in_file(a):   
+    for i in isabundant():
+        a.write(str(i) + "\n")
+        full.append(i)
 
 def search(n, l):
-    y = 390
+    y = len(l) - 1
     x = 0
     while l[x] + l[y] != n:
         #print('{} {} = {}'.format(x,y,l[x] + l[y]))
@@ -30,7 +37,7 @@ for i in range(1,1):
     if search(i,full):
         print(i)
         test += 1
-print(test)
 
-print(search(28124,full))
-print(full)
+print(lines)
+#print(search(28124,full))
+#print(full)
