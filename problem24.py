d7abacb60120f6c06f399ba8e960b59af176c070
Wishu <Wishu969@gmail.com>
2019-02-26 01:31:34 +0100

    solved problem24

--- problem24.py
+++ problem24.py
@@ -1,17 +1,23 @@
 #milionth lexicographic permutation
-def perm(n):
-    perml = ()
-    numlist = tuple(str(n))
-    x = len(numlist)
-    a = x-1
-    perml.__add__(x)
-    return x
+totalperm = []
+def perm(perm_list,length,totalperm):
+    if length <= 1:
+        temp = ""
+        for i in perm_list:
+            temp += str(i)
+        totalperm.append(temp)
+        #print(perm_list)
+        return 0
+    for number in range(0,length):
+        perm(perm_list,length - 1,totalperm)
+        if length % 2 != 0:
+            perm_list[0],perm_list[length-1] = perm_list[length-1],perm_list[0]
+        else:
+            perm_list[number], perm_list[length-1] = perm_list[length-1],perm_list[number]
 
-
-print(perm('0123'))
-#012
-#021
-#102
-#120
-#201
-#210
+test = [0,1,2,3,4,5,6,7,8,9]
+perm(test,len(test),totalperm)
+print('howdy')
+totalperm.sort()
+print(totalperm)
+print(totalperm[999999])
